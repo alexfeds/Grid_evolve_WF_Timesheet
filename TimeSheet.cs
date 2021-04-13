b96ea2437f23f737efc275ec3662a247055ff7a9
alexfeds <alexfeds@gmail.com>
2021-04-13 21:22:14 +0100

    Add project files.

---
+++ TimeSheet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Grid_evolve_WF_Timesheet
+{
+    public partial class TimeSheet : Form
+    {
+        public TimeSheet()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
